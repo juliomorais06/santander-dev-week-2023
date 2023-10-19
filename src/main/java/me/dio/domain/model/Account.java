b1891ac827ed70a693670560780c4ba8d556a80d
juliomorais06 <julio.fariasmorais@hotmail.com>
2023-10-19 17:03:02 -0300

    adicionado aplication-dev/prd

--- src/main/java/me/dio/domain/model/Account.java
+++ src/main/java/me/dio/domain/model/Account.java
@@ -16,10 +16,10 @@ public class Account {
 
     private String agency;
 
-    @Column(precision = 13, scale = 2)
+    @Column(precision = 2, scale = 13)
     private BigDecimal balance;
 
-    @Column(name = "additional_limit", precision = 13, scale = 2)
+    @Column(name = "additional_limit", precision = 2, scale = 13)
     private BigDecimal limit;
 
     public Long getId() {
